5adac81db016abc5cccc65663cd30cf424986ba0
thibault <thibault.blanchard@edu.itescia.fr>
2022-03-08 22:35:12 +0100

    Clean Update
    Update :
    - formulaire_test erase
    - function.js replace essai.js
    - formulaire.php clean

--- formulaire.php
+++ formulaire.php
@@ -15,6 +15,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/style-form.css">
+    <script src="./function.js"></script>
+    <script src="./race_ll.json"></script>
     <title>Legendary lord selection</title>
 </head>
 <body>
@@ -27,48 +29,26 @@
                     <form action="search.php" method="post">
                         <div id="label_option_race">
                             <label for="legendary_lord">Choose a legendary a race : </label>
-                            <select onchange="selectRace();" name="race" id="race">
+                            <select onchange="displayLegendaryLord('./race_ll.json')" name="race" id="race">
                                 <option disabled selected value> -- choose a race -- </option>
                                     <?php 
                                     while ($race = mysqli_fetch_array($resultat_race)) {
                                         echo '<option value="'.$race['race'].'">'.$race['race'].'</option>';
                                     }
-                                    ?>
-                                        <script>
-                                            function selectRace(){
-                                            var race = document.getElementById("race").value;
-                                            document.getElementById("raceDisplay").innerHTML="You selected "+race; 
-                                        }
+                                    ?>                                       
+                        </div>
+                            </select>
+                        <div id="label_option_ll">
+                            <label for="legendary_lord">Choose a legendary a legendary lord : </label>
+                                <select class="legendary_lord" id="legendary_lord" name="legendary_lord_selection">
+                                    <option disabled selected value> -- select a legendary lord -- </option>
+                                        <script> 
+                                            jsonFetch('./race_ll.json');
                                         </script>
+                                </select>
                         </div>
-                            </select>
-                            <div id="label_option_ll">
-                                <label for="legendary_lord">Choose a legendary a legendary lord : </label>
-                                    <select class="legendary_lord" id="legendary_lord" name="legendary_lord_selection">
-                                        <option disabled selected value> -- select a legendary lord -- </option>
-                                            <?php
-                                        
-                                                /*if (isset($race_selected) && $race_selected != 'value'){
-                                                $connect = new mysqli("localhost", "root", "root", "LegendaryLordsBonus");
-                                                $req_race_ll = "SELECT legendary_lord from race inner join legendarylords on race.id = legendarylords.race_id where race = $race";
-                                                $resultat_race_ll= $connect->query($req_race_ll);
-                                                $connect->close();
-                                                    while ($race_ll = mysqli_fetch_array($resultat_race_ll)) {
-                                                        echo '<option value="'.$race_ll['legendary_lord'].'">'.$race_ll['legendary_lord'].'</option>';
-
-                                                        }
-                                                }
-                                                else{*/
-                                                while ($ll = mysqli_fetch_array($resultat_ll)) {
-                                                    echo '<option value="'.$ll['legendary_lord'].'">'.$ll['legendary_lord'].'</option>';
-                                                }
-                                            
-                                            ?>
-                            </select>
-                        </div>
-                        <div id="container_button">
                             <input type="submit" value="confirm" id="button">
-                        </div>
+                        
                     </form>
                 <p id="raceDisplay"></p>
             </div>
--- function.js
+++ function.js
@@ -6,7 +6,7 @@ function selectRace(){
     document.getElementById("raceDisplay").innerHTML="You selected "+race; 
     };
 
-//Function which get data from JSON file and dispay the result in an option menu
+//Function which get data from JSON file and dispay the result in an option menu in function of the race
 function displayLegendaryLord(jsonFile){
     
     var race = document.getElementById("race").value;
@@ -14,7 +14,7 @@ function displayLegendaryLord(jsonFile){
     .then(results => results.json())
     .then((data) => {
         console.log(race)
-        var ll_Selection = document.getElementById("form-control");
+        var ll_Selection = document.getElementById("legendary_lord");
 
         for (var i = 0; i<data.length+1;i++){
             ll_Selection.remove('option'); 
@@ -36,6 +36,21 @@ function displayLegendaryLord(jsonFile){
         }   
    
     }); 
+    }
+    
+    //Function which get data from JSON file and dispay all legendary lords
+    function jsonFetch (jsonFile){
+        fetch(jsonFile)
+        .then(results => results.json())
+        .then((dataMain) => {
+            var ll_Selection = document.getElementById("legendary_lord");           
+            for (var i = 0; i<dataMain.length; i++){ 
+                var opt = document.createElement('option');     
+                opt.value= dataMain[i].legendary_lord;
+                opt.text = dataMain[i].legendary_lord;    
+                ll_Selection.appendChild(opt);
+            }
+        });
     }
 
    
