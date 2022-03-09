6665ad53febb5c74cc275d2fec0ee61cf03ebf17
thibault <thibault.blanchard@edu.itescia.fr>
2022-03-09 11:21:34 +0100

    Update function.js

--- function.js
+++ function.js
@@ -15,40 +15,33 @@ function displayLegendaryLord(jsonFile){
     .then((data) => {
         console.log(race)
         var ll_Selection = document.getElementById("legendary_lord");
-
         ll_Selection.remove('option'); 
-          
         for (var i = 0; i<data.length; i++){ 
-            var opt = document.createElement('option'); 
-
-            
-            if (data[i].race === race){  
-                 
+            var opt = document.createElement('option');   
+            if (data[i].race === race){                   
                 opt.value= data[i].legendary_lord;
                 opt.text = data[i].legendary_lord;    
-                ll_Selection.appendChild(opt);
-               
+                ll_Selection.appendChild(opt);             
             }
-            
         }   
-   
     }); 
     }
     
-    //Function which get data from JSON file and dispay all legendary lords
-    function jsonFetch (jsonFile){
-        fetch(jsonFile)
-        .then(results => results.json())
-        .then((dataMain) => {
-            var ll_Selection = document.getElementById("legendary_lord");           
-            for (var i = 0; i<dataMain.length; i++){ 
-                var opt = document.createElement('option');     
-                opt.value= dataMain[i].legendary_lord;
-                opt.text = dataMain[i].legendary_lord;    
-                ll_Selection.appendChild(opt);
-            }
-        });
-    }
+//Function which get data from JSON file and dispay all legendary lords
+function jsonFetch (jsonFile){
+    
+    fetch(jsonFile)
+    .then(results => results.json())
+    .then((dataMain) => {
+        var ll_Selection = document.getElementById("legendary_lord");           
+        for (var i = 0; i<dataMain.length; i++){ 
+            var opt = document.createElement('option');     
+            opt.value= dataMain[i].legendary_lord;
+            opt.text = dataMain[i].legendary_lord;    
+            ll_Selection.appendChild(opt);
+        }
+    });
+}
 
    
 
